729e5ccffc6d04789c6f592384d0ea21f9f30d7d
Shipon Roy <shiponworks@gmail.com>
2026-02-01 20:43:36 +0600

    Completes: day 1 problem solving

---
+++ 30 Days of JavaScript/Closures-Problems/problem-3-Closures/ExtraPratice.js
@@ -0,0 +1,34 @@
+// Extra Problems Closures
+
+//? Qs: 1 :
+/*
+ *Problem 1: Simple Counter
+
+Description:
+একটা counter বানাও, যা শুরু হবে 0 থেকে।
+
+প্রতিবার call করলে 1 বাড়বে
+
+একটা reset method থাকবে, যা counter আবার 0 করবে
+
+Example:
+
+const counter = createCounter();
+counter(); // 1
+counter(); // 2
+counter.reset();
+counter(); // 1
+ */
+// Ans:
+
+//? Qs: 2 :
+/*
+ *
+ */
+// Ans:
+
+//? Qs: 3 :
+/*
+ *
+ */
+// Ans:
--- 30 Days of JavaScript/Closures-Problems/problem-3-Closures/problem.js
+++ 30 Days of JavaScript/Closures-Problems/problem-3-Closures/problem.js
@@ -2,18 +2,98 @@
 
 //? Qs: 1 :
 /*
- *
+ *Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+Example 1:
+
+Input: args = []
+Output: "Hello World"
+Explanation:
+const f = createHelloWorld();
+f(); // "Hello World"
+
+The function returned by createHelloWorld should always return "Hello World".
+
+Example 2:
+
+Input: args = [{},null,42]
+Output: "Hello World"
+Explanation:
+const f = createHelloWorld();
+f({}, null, 42); // "Hello World"
+
+Any arguments could be passed to the function but it should still always return "Hello World".
+
  */
 // Ans:
+
+const createHelloWorld = function () {
+  return function () {
+    return 'Hello World';
+  };
+};
+const innerFunction = createHelloWorld();
+console.log(innerFunction());
 
 //? Qs: 2 :
 /*
- *
+ *Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+
+Example 1:
+
+Input: 
+n = 10 
+["call","call","call"]
+Output: [10,11,12]
+Explanation: 
+counter() = 10 // The first time counter() is called, it returns n.
+counter() = 11 // Returns 1 more than the previous time.
+counter() = 12 // Returns 1 more than the previous time.
+
+Example 2:
+
+Input: 
+n = -2
+["call","call","call","call","call"]
+Output: [-2,-1,0,1,2]
+Explanation: counter() initially returns -2. Then increases after each sebsequent call.
+ 
  */
 // Ans:
 
+const createCounter = function (n) {
+  let counter = n;
+  return function () {
+    let value = counter;
+    counter = counter + 1;
+    return counter;
+  };
+};
+console.log(createCounter(2));
+
 //? Qs: 3 :
 /*
- *
+ *Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+*toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+*notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+ 
+
+Example 1:
+
+Input: func = () => expect(5).toBe(5)
+Output: {"value": true}
+Explanation: 5 === 5 so this expression returns true.
+Example 2:
+
+Input: func = () => expect(5).toBe(null)
+Output: {"error": "Not Equal"}
+Explanation: 5 !== null so this expression throw the error "Not Equal".
+Example 3:
+
+Input: func = () => expect(5).notToBe(null)
+Output: {"value": true}
+Explanation: 5 !== null so this expression returns true.
  */
 // Ans:
