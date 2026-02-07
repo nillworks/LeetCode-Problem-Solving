562e9dd1c02bab77a3d7095f7d809acd6d4696eb
Shipon Roy <shiponworks@gmail.com>
2026-02-07 22:11:43 +0600

    Completes: compositoon problems 2 solve

---
+++ 30 Days of JavaScript/Function Transformations/script.js
@@ -0,0 +1,42 @@
+// Function Transformations
+function add(a, b) {
+  return a + b;
+}
+function mulTow(val) {
+  return 2 * val;
+}
+function square(val) {
+  return val * val;
+}
+
+function addSquare(a, b) {
+  return square(a, b);
+}
+
+// const result = add(2, 4);
+// console.log(square(result));
+
+function compose(f1, f2, f3) {
+  return function (a, b) {
+    return f3(f2(f1(a, b)));
+  };
+}
+
+const composeTow = (f1, f2, f3) => (a, b) => f3(f2(f1(a, b)));
+const result = compose(add, mulTow, addSquare);
+
+console.log(result(4, 4));
+
+// compose unlimit function
+
+function name(params) {
+  return function (x) {
+    for (let i = functions.length - 1; i >= 0; i--) {
+      x = functions[i](x);
+    }
+    return x;
+  };
+}
+
+var argumentsLength = (...args) => args.length;
+// var argumentsLength = function (...args) args.length;
