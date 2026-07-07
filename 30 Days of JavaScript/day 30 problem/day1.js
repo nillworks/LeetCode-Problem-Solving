0b9c1060e24f91d75b334ec817e805284bf3aa3a
Shipon Roy <shiponworks@gmail.com>
2026-07-07 15:58:33 +0600

    Add sleep function with promise-based delay

--- 30 Days of JavaScript/day 30 problem/day1.js
+++ 30 Days of JavaScript/day 30 problem/day1.js
@@ -13,3 +13,20 @@ var addTwoPromises = async function (promise1, promise2) {
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
  *   .then(console.log); // 4
  */
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, millis);
+  });
+}
+
+/**
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
